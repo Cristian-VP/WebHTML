b49b4c9cbd824a410fa19f773614ece182b709c9
Cristian-VP <cvacacelaprocel@cifpfibmoll.eu>
2025-02-02 18:59:40 +0100

    Validación formulario completa. Por revisar placeholder se me ve en mayus

--- js/components/form_register.js
+++ js/components/form_register.js
@@ -1,7 +1,6 @@
 
 //Expresiones regulares
-const regExUser = new RegExp('^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*$');
-//const regExPassword = new RegExp('^(?=.*[A-Za-z])(?=.*\\W)(?=.*\\d).{8,15}$');
+const regExUser = new RegExp('^[a-zA-Z]+([._-]?[a-zA-Z0-9]+)*$');
 const regExEmail     = new RegExp('^[\\w.-]{3,30}@[a-z]{3,30}\\.[a-z]{2,3}$');
 
 //DOM inputs
@@ -14,96 +13,114 @@ const errorPassword = document.getElementById('password-error');
 const errorUser = document.getElementById('user-error');
 
 // Functions validators inputs
-    //USER
-    function userValidation(input) {
-        console.log("Entro en la validación y soy user");
-        let value = input;
-        // Valido su longitud
-        if(value.length  < 4 || value.length > 10) {
-            errorUser.innerHTML = "Su longitud debe ser de 4 - 10 carácteres";
-            errorUser.style.display = "block";
-            return false;
-        }
-        //Valido su contenido
-        if(!regExUser.test(value)){
-            errorUser.innerHTML = "Solo puede contener: a-z A-Z 0-9 . - _";
-            errorUser.style.display = "block";
-            return false;
-        }
-        errorUser.style.display = "none";
-        userInput.style.border = "2px solid lightyellow";
-        return true;
-    }
     //EMAIL
     function emailValidation(input) {
         console.log("Entro en la validación y soy email");
-        let value = input;
+        let value = input.trim();
+        errorEmail.style.display = "none";
+        emailInput.style.border = "";
+
+        if(!input) {return false;}
         // Valido su longitud
         if(value.length  < 10 || value.length > 60) {
-            errorEmail.innerHTML = "Su longitud debe ser de 16 - 60 carácteres";
             errorEmail.style.display = "block";
+            errorEmail.innerHTML = "Su longitud debe ser de 16 - 60 carácteres";
             return false;
         }
         // Valido su formato
         if(!regExEmail.test(value)){
-            errorEmail.innerHTML = "No tiene el formato permitido. Solo puede contener: a-z A-Z 0-9 . - _ @";
             errorEmail.style.display = "block";
+            errorEmail.innerHTML = "No tiene el formato permitido. Debe contener: a-z A-Z 0-9 . - _ @";
             return false;
         }
-        errorEmail.style.display = "none";
-        emailInput.style.border = "2px solid lightyellow";
+
+        /*emailInput.style.border = "2px solid lightyellow";*/
         return true;
     }
+    //USER
+    function userValidation(input) {
+        console.log("Entro en la validación y soy user");
+        let value = input.trim();
+        errorUser.style.display = "none";
+        userInput.style.border = "";
+
+        if(!input) { return false; }
+        // Valido su longitud
+        if(value.length  < 4 || value.length > 10) {
+            errorUser.style.display = "block";
+            errorUser.innerHTML = "Su longitud debe ser de 4 - 10 carácteres";
+            return false;
+        }
+        //Valido su contenido
+        if(!regExUser.test(value)){
+            errorUser.style.display = "block";
+            errorUser.innerHTML = "Debe contener : a-z A-Z 0-9 . - _";
+            return false;
+        }
+
+        /*userInput.style.border = "2px solid lightyellow";*/
+        return true;
+    }
+
     //PASSWORD
     function passwordValidation(input) {
         console.log("Entro en la validación y soy password");
-        let value = input;
+        let value = input.trim();
+        errorPassword.style.display = "none";
+        passwordInput.style.border = "";
+
+
+        if(!input) { return false; }
         // Valido la longitud mínima
         if(value.length  < 8 || value.length > 15) {
-            errorPassword.innerHTML = "Debe de tener de 8 a 15 carácteres";
             errorPassword.style.display = "block";
+            errorPassword.innerHTML = "Debe de tener de 8 a 15 carácteres";
             return false;
         }
         // Lookhead positivo que asegura que exista al menos una letra [a-z]
         if(!/(?=.*[a-z])/i.test(value)){
-            errorPassword.innerHTML = "Tiene que tener al menos una letra";
             errorPassword.style.display = "block";
+            errorPassword.innerHTML = "Tiene que tener al menos una letra";
             return false;
         }
         // Lookhead positivo que asegura que exista al menos un carácter especial [\W]
         if(!/(?=.*\W)/.test(value)){
-            errorPassword.innerHTML = "Tiene que tener al menos un crácter especial";
             errorPassword.style.display = "block";
+            errorPassword.innerHTML = "Tiene que tener al menos un carácter especial";
             return false;
         }
         // Lookhead positivo que asegura que exista al menos un número positivo [0-9]
         if(!/(?=.*\d)/.test(value)){
-            errorPassword.innerHTML = "Tiene que tener al menos un número entero positivo";
             errorPassword.style.display = "block";
+            errorPassword.innerHTML = "Tiene que tener al menos un número entero positivo";
             return false;
         }
-        errorPassword.style.display = "none";
-        passwordInput.style.border = "2px solid lightyellow";
+
+        /*passwordInput.style.border = "2px solid lightyellow";*/
         return true;
     }
 
     //Function validatos form
-    export function formValidator (){
+    export function formValidator () {
         let userInputValue = userInput.value;
         let passwordInputValue = passwordInput.value;
         let emailInputValue = emailInput.value;
 
-        const userInputValidated = userValidation(userInputValue) ;
+        const emailInputValidated =  emailValidation(emailInputValue);
+        const userInputValidated =  userValidation(userInputValue) ;
         const passwordInputValidated = passwordValidation(passwordInputValue);
-        const emailInputValidated = emailValidation(emailInputValue);
 
-        if( userInputValidated && passwordInputValidated && emailInputValidated){
-            emailInput.style.borderColor = "green";
-            userInput.style.borderColor = "green";
-            passwordInput.style.borderColor = "green";
+        if( emailInputValidated && userInputValidated && passwordInputValidated ){
+
+            emailInput.style.border = "2px solid var(--color-green-letter)";
+            userInput.style.border = "2px solid var(--color-green-letter)";
+            passwordInput.style.border = "2px solid var(--color-green-letter)";
 
             buttonSubmit.removeAttribute('disabled');
             buttonSubmit.setAttribute('enable', 'true');
+        }else{
+            buttonSubmit.removeAttribute('enabled');
+            buttonSubmit.setAttribute('disabled', 'true');
         }
     }
 
--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
-//import './components/form_register.js';
 import {formValidator} from "./components/form_register.js";
 
 const emailInput = document.getElementById('email');
 const userInput = document.getElementById('user');
 const passwordInput = document.getElementById('password');
 
-emailInput.addEventListener('input', formValidator);
-userInput.addEventListener('input', formValidator);
-password.addEventListener('input', formValidator);
+emailInput.addEventListener('input', () =>{
+    formValidator();
+});
+userInput.addEventListener('input', ()=>{
+    formValidator();
+});
+passwordInput.addEventListener('input', () => {
+    formValidator();
+});
 
 //userInput.addEventListener('input', formValidator);*/
